62d2a0150cab0c77ce61349f0a5928891f9e183d
Matthew Vandenberg <mv5903@gmail.com>
2023-10-26 19:32:22 -0400

    Added textcodeinput and button components

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,5 @@
+export default function Button({ text }: { text: string }) {
+    return (
+        <button className="text-white text-center bg-secondary border-2 border-[#017BAF] rounded-[10px] w-[227px] h-[55px] font-secondary font-bold text-[18px]">{text}</button>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+        tertiary: 'var(--tertiary-color)',
+      },
+      fontFamily: {
+        primary: 'var(--primary-font)',
+        secondary: 'var(--secondary-font)', 
+      }
     },
   },
   plugins: [],
