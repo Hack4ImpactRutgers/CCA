fc4b1c52463436601a1b246d8859905e4e78128a
OGreeni <omri.green1@gmail.com>
2023-11-17 15:22:20 -0500

    header fixes

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
-
+import Link from 'next/link'
+import {usePathname} from "next/navigation";
 
 
 export default function Header() {
   const pathname = usePathname()
+
   return (
     <nav className='bg-[var(--primary-color)] no-underline'>
-        
+
         <ul className='p-0 mt-0 h-20 [list-style:none] flex items-center gap-4'>
             <img className='ml-[.5rem] text-[large] no-underline text-[rgb(255,_255,_255)]' src="../../favicon.ico"/>
-            
+
             <Link className={`text-[large] no-underline text-[rgb(255,_255,_255)] hover:bg-[var(--secondary-color)] ${
                   pathname=='volunteer'
                     ? "text-[var(--tertiary-color)] font-bold"
                     : ""
                 }`} href="/volunteer">Volunteer</Link>
 
-                
+
               <Link className={`text-[large] no-underline text-[rgb(255,_255,_255)] hover:bg-[var(--secondary-color)] ${
-                  pathname=='client'
-                    ? "text-[var(--tertiary-color)] font-bold"
-                    : ""
+                  pathname=='client' && "text-[var(--tertiary-color)] font-bold"
+                    
                 }`} href="/client">Client</Link>
             <Link className={`text-[large] no-underline text-[rgb(255,_255,_255)] hover:bg-[var(--secondary-color)] ${
                   pathname=='contact'
