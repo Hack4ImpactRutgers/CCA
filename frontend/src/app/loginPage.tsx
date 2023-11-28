d7c04b5731edcb299d087c95c278f553f6ab3fa4
Aashita Pandey <aashiyaaaa14@gmail.com>
2023-11-28 14:11:07 -0500

    fixed to match design team's adjustments

--- frontend/src/app/loginPage.tsx
+++ frontend/src/app/loginPage.tsx
@@ -11,8 +11,12 @@ import image1 from '/public/svg/image1.png';
 
 
 export default function LoginPage() {
-  const [textInput, setTextInput] = useState("");
-  const [selectedOption, setSelectedOption] = useState("");
+  const [userTextInput, setUserTextInput] = useState("");
+  const [passTextInput, setPassTextInput] = useState("");
+  const [nameTextInput, setNameTextInput] = useState("");
+  const [phoneTextInput, setPhoneTextInput] = useState("");
+  const [emailTextInput, setEmailTextInput] = useState("");
+  const [selectedOption, setSelectedOption] = useState("admin");
 
   const handleAdminClick = (): void =>{
     setSelectedOption("admin");
@@ -20,8 +24,45 @@ export default function LoginPage() {
   const handleVolunteerClick = (): void =>{
     setSelectedOption("volunteer");
   }
+  
+  const forgotPassword = (): void =>{
+    setSelectedOption("forgotPass")
+  }
+  const volSignUp = (): void =>{
+    setSelectedOption("volSignUp")
+  }
 
+  const admLoginButton = (): void =>{
+    setSelectedOption("admLoggingIn")
+  }
+
+  const volunteerConfirm = (): void =>{
+    setSelectedOption("volLogInConfirm")
+  }
+
+  const volunteerSigningUpConfirm = (): void =>{
+    setSelectedOption("volSignUpConfirm")
+  }
+
+    const handleLoginKey = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      // Trigger the click event of the login button
+      if (selectedOption == 'admin') {
+        admLoginButton();
+      }
+      if (selectedOption == 'volunteer') {
+        volunteerConfirm();
+      }
+      if (selectedOption == 'volSignUp') {
+        volunteerSigningUpConfirm();
+      }
+
+    }
+  };
+
+  
   return (
+    
     <div id="fullPage">
       {/* 
       <TextInput placeholder={"awdad"} value={textInput} onChange={setTextInput} />
@@ -46,42 +87,168 @@ export default function LoginPage() {
 
           
         </div>
+
+
         <div style={{height:"fit-content", width:"100%", float:"left"}}>
           <div id="image">
             <Image src={image1} alt="image" layout="responsive" width={300} height={300} />
           </div>
         </div>
 
+
+
+
+
+
+
+
         {selectedOption === "admin" && (
+          <div>
+
+
           <div id="adminContent">
-            {/* Content specific to the Admin option */}
             <div style={{margin: 'auto', height:"fit-content", width:"fit-content"}}><h1>Account Login</h1></div>
               <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="userInput">
-                <TextInput placeholder={"Username"} value={textInput} onChange={setTextInput}/>
+                <TextInput placeholder={"Username"} value={userTextInput} onChange={setUserTextInput}/>
               </div>
               <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="passwordInput">
-                <TextInput placeholder={"Password"} value={textInput} onChange={setTextInput}/>
+                <TextInput placeholder={"Password"} value={passTextInput} onChange={setPassTextInput}/>
             </div>
+          </div>
+
+          <div id="smallLinks" >
+            <button onClick={forgotPassword}>
+              Forgot Username or Password?
+            </button>
+          </div>
+
+          <div id="logButton">
+            <Button onClick={admLoginButton} text={"Login"}></Button>
+          </div>
+
+
           </div>
         )}
 
+
+        {selectedOption === "admLoggingIn" && (
+          <div>
+
+
+          admLoggingIn TBD
+
+
+          </div>
+        )}
+
+
+
+
+
+
+
+
         {selectedOption === "volunteer" && (
+          <div>
+
+
           <div id="volunteerContent">
-            {/* Content specific to the Volunteer option */}
-            <div style={{margin: 'auto', height:"fit-content", width:"fit-content"}}><h1>Account Login</h1></div>
-              <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="passwordInput">
-                <TextInput placeholder={"Password"} value={textInput} onChange={setTextInput}/>
+            <div style={{margin: 'auto', height:"fit-content", width:"fit-content"}}><h1>Login</h1></div>
+              
+            <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="nameInput">
+              <TextInput placeholder={"Name *"} value={nameTextInput} onChange={setNameTextInput}/>
             </div>
+
+            <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="emailInput">
+              <TextInput placeholder={"Email *"} value={emailTextInput} onChange={setEmailTextInput}/>
+            </div>
+
+          </div>
+
+          <div id="smallLinks" >
+            <button onClick={volSignUp}>
+              Signup
+            </button>
+          </div>
+
+          <div id="logButton">
+            <Button onClick={volunteerConfirm} text={"Confirm"}></Button>
+          </div>
+
+
+        </div>
+        )}
+
+        {selectedOption === "volLogInConfirm" && (
+          <div>
+
+
+          volLogInConfirm TBD
+
+
+          </div>
+        )}
+
+
+
+
+
+
+        {selectedOption === "forgotPass" && (
+          <div>
+
+            
+          <div id="forgotPassContent">
+            forgot pass! TBD
+
+          </div>
+
+
+          </div>
+        )}
+
+
+
+
+
+
+
+        {selectedOption === "volSignUp" && (
+          <div>
+
+
+          <div id="volSignUpContent">
+            <div style={{margin: 'auto', height:"fit-content", width:"fit-content"}}><h1>Sign Up</h1></div>
+            
+            <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="nameInput">
+              <TextInput placeholder={"Name *"} value={nameTextInput} onChange={setNameTextInput}/>
+            </div>
+
+            <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="numberInput">
+              <TextInput placeholder={"Phone Number *"} value={phoneTextInput} onChange={setPhoneTextInput}/>
+            </div>
+
+            <div style={{width:'fit-content', margin:'auto', marginTop:'4%', marginBottom:'4%'}} id="emailInput">
+              <TextInput placeholder={"Email *"} value={emailTextInput} onChange={setEmailTextInput}/>
+            </div>
+          </div>
+
+          <div id="logButton">
+            <Button onClick={volunteerSigningUpConfirm} text={"Confirm"}></Button>
+          </div>
+
+
+          </div>
+        )}
+
+        {selectedOption === "volSignUpConfirm" && (
+          <div style={{margin: 'auto', height:"fit-content", width:"100%", float:"left", marginTop:"50px"}}>
+            <h1>Confirmed</h1>
           </div>
         )}
 
         
-        <div id="forPass">
-          Forgot Username or Password?
-        </div>
-        <div id="logButton">
-          <Button text={"Login"}></Button>
-        </div>
+        
         
       </div>
 
