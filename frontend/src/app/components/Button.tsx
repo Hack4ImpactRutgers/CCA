50e7b96f0eeb632e04fb377ef34dc13549ac5a59
Aashita Pandey <aashiyaaaa14@gmail.com>
2023-11-28 14:12:52 -0500

    fixed to match design team's adjustments

--- frontend/src/app/components/Button.tsx
+++ frontend/src/app/components/Button.tsx
@@ -1,5 +1,16 @@
-export default function Button({ text }: { text: string }) {
+"use client";
+
+interface ButtonProps {
+    text: string;
+    onClick: () => void;
+  }
+
+export default function Button(props:ButtonProps) {
+    const { text, onClick} = props;
+
     return (
-        <button className="text-white text-center bg-secondary border-2 border-[#017BAF] rounded-[10px] w-[227px] h-[55px] font-secondary font-bold text-[18px] hover:bg-[#017BAF]">{text}</button>
+        <button onClick={onClick} className="text-white text-center bg-secondary border-2 border-[#017BAF] rounded-[10px] w-[227px] h-[55px] font-secondary font-bold text-[18px] hover:bg-[#017BAF]">
+            {text}
+        </button>
     );
 }
