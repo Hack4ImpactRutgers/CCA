51a092d29f3695ef7f2b93b877b26f723be1d23e
OGreeni <omri.green1@gmail.com>
2024-01-10 22:23:24 -0500

    add prettier config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        tertiary: "var(--tertiary-color)",
+      },
+      fontFamily: {
+        primary: "var(--primary-font)",
+        secondary: "var(--secondary-font)",
       },
     },
   },
