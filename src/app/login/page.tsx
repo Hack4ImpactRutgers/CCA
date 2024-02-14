c2256ffd17c41ae70d578ba61b347e8c298265ca
aashiyaaaa <82184492+aashiyaaaa@users.noreply.github.com>
2024-02-14 12:20:38 -0500

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,8 +39,8 @@ export default function LoginPage() {
         setSelectedOption('volLogInConfirm');
     };
     const volunteerConfirm2 = (): void => {
-      setSelectedOption('volLogInConfirm2');
-  };
+        setSelectedOption('volLogInConfirm2');
+    };
 
     const volunteerSigningUpConfirm = (): void => {
         setSelectedOption('volSignUpConfirm');
@@ -72,12 +72,12 @@ export default function LoginPage() {
       <TextInput placeholder={"awdad"} value={textInput} onChange={setTextInput} />
       <Button text={"awdad"}></Button>
       */}
-            <div className="h-10 min-h-10 bg-primary-color">
+            <div className="bg-primary-color h-10 min-h-10">
                 <Navbar />
             </div>
 
-            <div className="w-auto h-96 m-auto">
-                <div className="flex justify-center items-center h-auto w-auto mt-10">
+            <div className="m-auto h-96 w-auto">
+                <div className="mt-10 flex h-auto w-auto items-center justify-center">
                     <div className="mr-1">
                         <LoginButton
                             text={'Admin'}
@@ -95,7 +95,7 @@ export default function LoginPage() {
                     </div>
                 </div>
 
-                <div className="flex justify-center items-center">
+                <div className="flex items-center justify-center">
                     <div className="h-239 w-239 mx-auto">
                         <Image
                             src={image1}
@@ -107,25 +107,21 @@ export default function LoginPage() {
                 </div>
 
                 {selectedOption === 'admin' && (
-                    <div  className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col items-center justify-center">
                         <div id="adminContent">
                             <div>
-                                <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Account Login</h1>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Account Login
+                                </h1>
                             </div>
-                            <div
-                                className="mt-4 mb-4"
-                                id="userInput"
-                            >
+                            <div className="mb-4 mt-4" id="userInput">
                                 <TextInput
                                     placeholder={'Username'}
                                     value={userTextInput}
                                     onChange={setUserTextInput}
                                 />
                             </div>
-                            <div
-                                className="mt-4 mb-4"
-                                id="passwordInput"
-                            >
+                            <div className="mb-4 mt-4" id="passwordInput">
                                 <TextInput
                                     placeholder={'Password'}
                                     value={passTextInput}
@@ -134,7 +130,7 @@ export default function LoginPage() {
                             </div>
                         </div>
 
-                        <div className="font-bold font-roboto text-xs mx-auto w-full text-center pb-3 text-secondary">
+                        <div className="font-roboto mx-auto w-full pb-3 text-center text-xs font-bold text-secondary">
                             <button onClick={forgotPassword}>
                                 Forgot Username or Password?
                             </button>
@@ -151,23 +147,24 @@ export default function LoginPage() {
 
                 {selectedOption === 'admLoggingIn' && (
                     <div>
-                        <div className="flex flex-col justify-center items-center">
-                            <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Confirmed</h1>
+                        <div className="flex flex-col items-center justify-center">
+                            <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                Confirmed
+                            </h1>
                         </div>
                     </div>
                 )}
 
                 {selectedOption === 'volunteer' && (
-                    <div className="flex flex-col justify-center items-center">
-                        <div id="volunteerContent" >
+                    <div className="flex flex-col items-center justify-center">
+                        <div id="volunteerContent">
                             <div>
-                                <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Login</h1>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Login
+                                </h1>
                             </div>
 
-                            <div 
-                                className="mt-4 mb-4"
-                                id="nameInput"
-                            >
+                            <div className="mb-4 mt-4" id="nameInput">
                                 <TextInput
                                     placeholder={'Name *'}
                                     value={nameTextInput}
@@ -175,10 +172,7 @@ export default function LoginPage() {
                                 />
                             </div>
 
-                            <div
-                                className="mt-4 mb-4"
-                                id="emailInput"
-                            >
+                            <div className="mb-4 mt-4" id="emailInput">
                                 <TextInput
                                     placeholder={'Email *'}
                                     value={emailTextInput}
@@ -187,7 +181,7 @@ export default function LoginPage() {
                             </div>
                         </div>
 
-                        <div className="font-bold font-roboto text-xs mx-auto w-full text-center pb-3 text-secondary">
+                        <div className="font-roboto mx-auto w-full pb-3 text-center text-xs font-bold text-secondary">
                             <button onClick={volSignUp}>Signup</button>
                         </div>
 
@@ -201,10 +195,10 @@ export default function LoginPage() {
                 )}
 
                 {selectedOption === 'volLogInConfirm' && (
-                    <div  className="flex flex-col justify-center items-center">
-                        <div className="mx-auto text-center mt-4">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="mx-auto mt-4 text-center">
                             Enter the code sent to your email
-                            <div className="mt-4 mb-4">
+                            <div className="mb-4 mt-4">
                                 <CodeInput />
                             </div>
                         </div>
@@ -218,32 +212,35 @@ export default function LoginPage() {
                 )}
 
                 {selectedOption === 'volLogInConfirm2' && (
-                    <div className="flex flex-col justify-center items-center">
-                      <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Logged In!</h1>
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                            Logged In!
+                        </h1>
                     </div>
                 )}
 
                 {selectedOption === 'forgotPass' && (
                     <div>
                         <div id="forgotPassContent">
-                            <div className="flex flex-col justify-center items-center">
-                                <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">forgot pass! TBD</h1>
+                            <div className="flex flex-col items-center justify-center">
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    forgot pass! TBD
+                                </h1>
                             </div>
                         </div>
                     </div>
                 )}
 
                 {selectedOption === 'volSignUp' && (
-                    <div  className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col items-center justify-center">
                         <div id="volSignUpContent">
                             <div>
-                                <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Sign Up</h1>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Sign Up
+                                </h1>
                             </div>
 
-                            <div
-                                className="mt-4 mb-4"
-                                id="nameInput"
-                            >
+                            <div className="mb-4 mt-4" id="nameInput">
                                 <TextInput
                                     placeholder={'Name *'}
                                     value={nameTextInput}
@@ -251,10 +248,7 @@ export default function LoginPage() {
                                 />
                             </div>
 
-                            <div
-                                className="mt-4 mb-4"
-                                id="numberInput"
-                            >
+                            <div className="mb-4 mt-4" id="numberInput">
                                 <TextInput
                                     placeholder={'Phone Number *'}
                                     value={phoneTextInput}
@@ -262,10 +256,7 @@ export default function LoginPage() {
                                 />
                             </div>
 
-                            <div
-                                className="mt-4 mb-4"
-                                id="emailInput"
-                            >
+                            <div className="mb-4 mt-4" id="emailInput">
                                 <TextInput
                                     placeholder={'Email *'}
                                     value={emailTextInput}
@@ -284,9 +275,10 @@ export default function LoginPage() {
                 )}
 
                 {selectedOption === 'volSignUpConfirm' && (
-                    <div className="flex flex-col justify-center items-center"
-                    >
-                        <h1 className="font-bold font-roboto text-2xl text-center mt-2 mb-2">Confirmed</h1>
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                            Confirmed
+                        </h1>
                     </div>
                 )}
             </div>
