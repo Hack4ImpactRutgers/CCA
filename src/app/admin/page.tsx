3334de24974e96e94008608aad1c05c0c4e8e174
Matthew Vandenberg <mv5903@gmail.com>
2024-02-14 00:11:01 -0500

    admin clients (#60)
    * admin clients
    
    * fix formatting
    
    ---------
    
    Co-authored-by: OGreeni <omri.green1@gmail.com>

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import Dashboard from './Dashboard';
+
+interface PageProps {}
+
+const Page: FC<PageProps> = () => {
+    return (
+        <main>
+            <Dashboard />
+        </main>
+    );
+};
+
+export default Page;
