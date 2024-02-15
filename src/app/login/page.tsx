707b1a8768921a6e294f277c87ca285dd085c528
aashiyaaaa <82184492+aashiyaaaa@users.noreply.github.com>
2024-02-15 10:55:28 -0500

    updated login (#59)
    * updated login
    
    updated login that has the input code option
    
    * Create login.css
    
    * Update page.tsx
    
    * Create login.css
    
    * Update login.css
    
    * Update login.css
    
    * Update page.tsx
    
    * Delete src/app/login/login.css
    
    * Update page.tsx
    
    * Delete src/app/login.css
    
    * Update page.tsx
    
    * Update page.tsx
    
    * Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,64 +1,287 @@
 'use client';
 import { FC, useState } from 'react';
-import Image from 'next/image';
 import { TextInput } from '@/components/core/TextInput';
-import { LoginButton } from '@/components/auth/LoginButton';
 import { Button } from '@/components/core/Button';
+import { LoginButton } from '@/components/auth/LoginButton';
+import { Navbar } from '@/components/core/Navbar';
+import Image from 'next/image';
+import image1 from '/public/images/logo.png';
 import Link from 'next/link';
+import { CodeInput } from '@/components/auth/CodeInput';
 
-interface PageProps {}
+export default function LoginPage() {
+    const [userTextInput, setUserTextInput] = useState('');
+    const [passTextInput, setPassTextInput] = useState('');
+    const [nameTextInput, setNameTextInput] = useState('');
+    const [phoneTextInput, setPhoneTextInput] = useState('');
+    const [emailTextInput, setEmailTextInput] = useState('');
+    const [selectedOption, setSelectedOption] = useState('admin');
 
-const Page: FC<PageProps> = () => {
-    const [usernameInput, setUsernameInput] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
-    const [loginType, setLoginType] = useState<'ADMIN' | 'VOLUNTEER'>('ADMIN');
+    const handleAdminClick = (): void => {
+        setSelectedOption('admin');
+    };
+    const handleVolunteerClick = (): void => {
+        setSelectedOption('volunteer');
+    };
+
+    const forgotPassword = (): void => {
+        setSelectedOption('forgotPass');
+    };
+    const volSignUp = (): void => {
+        setSelectedOption('volSignUp');
+    };
+
+    const admLoginButton = (): void => {
+        setSelectedOption('admLoggingIn');
+    };
+
+    const volunteerConfirm = (): void => {
+        setSelectedOption('volLogInConfirm');
+    };
+    const volunteerConfirm2 = (): void => {
+        setSelectedOption('volLogInConfirm2');
+    };
+
+    const volunteerSigningUpConfirm = (): void => {
+        setSelectedOption('volSignUpConfirm');
+    };
+
+    const handleLoginKey = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ): void => {
+        if (event.key === 'Enter') {
+            // Trigger the click event of the login button
+            if (selectedOption == 'admin') {
+                admLoginButton();
+            }
+            if (selectedOption == 'volunteer') {
+                volunteerConfirm();
+            }
+            if (selectedOption == 'volunteer2') {
+                volunteerConfirm2();
+            }
+            if (selectedOption == 'volSignUp') {
+                volunteerSigningUpConfirm();
+            }
+        }
+    };
 
     return (
-        <main>
-            <div className="mt-52 flex flex-col items-center gap-6">
-                <div>
-                    <LoginButton
-                        text="Admin"
-                        onClick={() => setLoginType('ADMIN')}
-                        active={loginType === 'ADMIN'}
-                    />
-                    <LoginButton
-                        text="Volunteer"
-                        onClick={() => setLoginType('VOLUNTEER')}
-                        active={loginType === 'VOLUNTEER'}
-                    />
-                </div>
-                <Image
-                    src="/images/logo.png"
-                    alt="logo"
-                    width={239}
-                    height={239}
-                />
-                <span className="text-3xl font-bold text-tertiary">
-                    Account Login
-                </span>
-                <TextInput
-                    value={usernameInput}
-                    onChange={setUsernameInput}
-                    placeholder={'Username'}
-                />
-                <TextInput
-                    value={passwordInput}
-                    onChange={setPasswordInput}
-                    placeholder={'Password'}
-                />
-                <div className="flex flex-col gap-2">
-                    <Link
-                        className="text-sm text-[#017BAF] hover:underline"
-                        href="/"
-                    >
-                        Forgot Username or Password
-                    </Link>
-                    <Button text={'Log In'} onClick={() => {}} />
-                </div>
+        <div id="fullPage" className="bg-white">
+            {/* 
+      <TextInput placeholder={"awdad"} value={textInput} onChange={setTextInput} />
+      <Button text={"awdad"}></Button>
+      */}
+            <div className="bg-primary-color h-10 min-h-10">
+                <Navbar />
             </div>
-        </main>
-    );
-};
 
-export default Page;
+            <div className="m-auto h-96 w-auto">
+                <div className="mt-10 flex h-auto w-auto items-center justify-center">
+                    <div className="mr-1">
+                        <LoginButton
+                            text={'Admin'}
+                            onClick={handleAdminClick}
+                            active={true}
+                        />
+                    </div>
+
+                    <div className="ml-1">
+                        <LoginButton
+                            text={'Volunteer'}
+                            onClick={handleVolunteerClick}
+                            active={true}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <div className="h-239 w-239 mx-auto">
+                        <Image
+                            src={image1}
+                            alt="image"
+                            width={239}
+                            height={239}
+                        />
+                    </div>
+                </div>
+
+                {selectedOption === 'admin' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <div id="adminContent">
+                            <div>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Account Login
+                                </h1>
+                            </div>
+                            <div className="mb-4 mt-4" id="userInput">
+                                <TextInput
+                                    placeholder={'Username'}
+                                    value={userTextInput}
+                                    onChange={setUserTextInput}
+                                />
+                            </div>
+                            <div className="mb-4 mt-4" id="passwordInput">
+                                <TextInput
+                                    placeholder={'Password'}
+                                    value={passTextInput}
+                                    onChange={setPassTextInput}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="font-roboto mx-auto w-full pb-3 text-center text-xs font-bold text-secondary">
+                            <button onClick={forgotPassword}>
+                                Forgot Username or Password?
+                            </button>
+                        </div>
+
+                        <div id="logButton">
+                            <Button
+                                onClick={admLoginButton}
+                                text={'Login'}
+                            ></Button>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'admLoggingIn' && (
+                    <div>
+                        <div className="flex flex-col items-center justify-center">
+                            <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                Confirmed
+                            </h1>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'volunteer' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <div id="volunteerContent">
+                            <div>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Login
+                                </h1>
+                            </div>
+
+                            <div className="mb-4 mt-4" id="nameInput">
+                                <TextInput
+                                    placeholder={'Name *'}
+                                    value={nameTextInput}
+                                    onChange={setNameTextInput}
+                                />
+                            </div>
+
+                            <div className="mb-4 mt-4" id="emailInput">
+                                <TextInput
+                                    placeholder={'Email *'}
+                                    value={emailTextInput}
+                                    onChange={setEmailTextInput}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="font-roboto mx-auto w-full pb-3 text-center text-xs font-bold text-secondary">
+                            <button onClick={volSignUp}>Signup</button>
+                        </div>
+
+                        <div id="logButton" className="m-auto">
+                            <Button
+                                onClick={volunteerConfirm}
+                                text={'Confirm'}
+                            ></Button>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'volLogInConfirm' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="mx-auto mt-4 text-center">
+                            Enter the code sent to your email
+                            <div className="mb-4 mt-4">
+                                <CodeInput />
+                            </div>
+                        </div>
+                        <div id="logButton">
+                            <Button
+                                onClick={volunteerConfirm2}
+                                text={'Confirm'}
+                            ></Button>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'volLogInConfirm2' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                            Logged In!
+                        </h1>
+                    </div>
+                )}
+
+                {selectedOption === 'forgotPass' && (
+                    <div>
+                        <div id="forgotPassContent">
+                            <div className="flex flex-col items-center justify-center">
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    forgot pass! TBD
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'volSignUp' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <div id="volSignUpContent">
+                            <div>
+                                <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                                    Sign Up
+                                </h1>
+                            </div>
+
+                            <div className="mb-4 mt-4" id="nameInput">
+                                <TextInput
+                                    placeholder={'Name *'}
+                                    value={nameTextInput}
+                                    onChange={setNameTextInput}
+                                />
+                            </div>
+
+                            <div className="mb-4 mt-4" id="numberInput">
+                                <TextInput
+                                    placeholder={'Phone Number *'}
+                                    value={phoneTextInput}
+                                    onChange={setPhoneTextInput}
+                                />
+                            </div>
+
+                            <div className="mb-4 mt-4" id="emailInput">
+                                <TextInput
+                                    placeholder={'Email *'}
+                                    value={emailTextInput}
+                                    onChange={setEmailTextInput}
+                                />
+                            </div>
+                        </div>
+
+                        <div id="logButton">
+                            <Button
+                                onClick={volunteerSigningUpConfirm}
+                                text={'Confirm'}
+                            ></Button>
+                        </div>
+                    </div>
+                )}
+
+                {selectedOption === 'volSignUpConfirm' && (
+                    <div className="flex flex-col items-center justify-center">
+                        <h1 className="font-roboto mb-2 mt-2 text-center text-2xl font-bold">
+                            Confirmed
+                        </h1>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
