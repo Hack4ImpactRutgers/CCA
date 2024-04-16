aa58c4f986fc4636bc5ef124a43d0f18145ddb6a
OGreeni <omri.green1@gmail.com>
2024-04-16 15:14:33 -0400

    tmp

--- frontend/src/app/delivery-form/page.tsx
+++ frontend/src/app/delivery-form/page.tsx
@@ -51,7 +51,7 @@ export default function DeliveryReportForm() {
         try {
             // Fetch all clients
             const response = await fetch(
-                `${process.env.NEXT_PUBLIC_API_BASE_URL}/clients/all`,
+                `${process.env.NEXT_PUBLIC_API_BASE_URL}/client/all`,
                 {
                     credentials: 'include', // Include credentials
                     headers: {
